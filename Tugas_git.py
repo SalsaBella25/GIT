f7ef1a9ec3ca792a43649697c2caf7f73eb16377
Nazwa Nur Salsa Bella <nazwa.nur@mhs.itenas.ac.id>
2024-12-05 13:33:47 +0700

    soal dan jawaban no 1,2,3

---
+++ Tugas_git.py
@@ -0,0 +1,56 @@
+data_panen = {
+    'lokasi1': {
+        'nama_lokasi':'kebun A',
+        'hasil_panen': {
+            'padi':1200,
+            'jagung':800,
+            'kedelai':500
+        }
+    },
+    'lokasi2': {
+        'nama_lokasi':'kebun B',
+        'hasil_panen': {
+            'padi':1500,
+            'jagung':900,
+            'kedelai':450
+        }
+    },
+    'lokasi3': {
+        'nama_lokasi':'kebun C',
+        'hasil_panen': {
+            'padi':1100,
+            'jagung':750,
+            'kedelai':600
+        }
+    },
+    'lokasi4': {
+        'nama_lokasi':'kebun D',
+        'hasil_panen': {
+            'padi':1300,
+            'jagung':850,
+            'kedelai':550
+        }
+    },
+    'lokasi5': {
+        'nama_lokasi':'kebun E',
+        'hasil_panen': {
+            'padi':1400,
+            'jagung':950,
+            'kedelai':480
+        }
+    }
+}
+
+for lokasi, data in data_panen.items():
+    print(f"nama lokasi:{data['nama_lokasi']}")
+    print('hasil_panen:')
+    for hasil, jumlah in data['hasil_panen'].items():
+        print(f"{hasil}:{jumlah}")
+    print()
+
+jumlahJagung_lokasi2 = data_panen['lokasi2']['hasil_panen']['jagung']
+print(f"jumlah hasil panen jagung dari lokasi2 adalah: {jumlahJagung_lokasi2}")
+
+namaLokasi_lokasi3 = data_panen['lokasi3']['nama_lokasi']
+print(f"nama dari lokasi3 adalah: {namaLokasi_lokasi3}")
+
